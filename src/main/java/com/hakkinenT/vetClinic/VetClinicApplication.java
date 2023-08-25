023e7813a33ac85f73d94db03e951cf3b97af702
Tawanna Hakkinen <thol_16@hotmail.com>
2023-08-25 14:35:28 -0300

    Project created

---
+++ src/main/java/com/hakkinenT/vetClinic/VetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.vetClinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hakkinenT/vetClinic/VetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hakkinenT.vetClinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
